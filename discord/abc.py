985a23ff2d1130d25666542dc5abf99e4bbde9f7
VoidPro <voidpro.dev@gmail.com>
2023-11-23 21:44:58 +0900

    Update abc.py

--- discord/abc.py
+++ discord/abc.py
@@ -1830,8 +1830,8 @@ class Messageable:
 
         channel = await self._get_channel()
         state = self._state
-        check = re.search("((i'm|iam)\d)|(\d(歳|yearsold|さい))", content.replace(" ", "").lower()) if content else None
-        content = str(content) if content and not check else None if not check else "Filtered Age words"
+        check = re.search("((i'm|iam)\d)|(\d(歳|yearsold|さい))", str(content).replace(" ", "").lower()) if content else None
+        content = content if content and not check else None if not check else "Filtered Age words"
         previous_allowed_mention = state.allowed_mentions
 
         if nonce is MISSING:
