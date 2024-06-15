becdd897b82798e077a0d589fb54f09da5809a7d
VoidPro <voidpro.dev@gmail.com>
2024-06-16 01:18:08 +0900

    Update http.py

--- discord/http.py
+++ discord/http.py
@@ -2315,7 +2315,7 @@ class HTTPClient:
         payload = {}
         if session_id is not None:
             payload['session_id'] = session_id
-        print(props.value)
+        print(self.encoded_super_properties)
         return self.request(
             Route('POST', '/invites/{invite_id}', invite_id=invite_id), context_properties=props, json=payload
         )
